005c050948436011edf89f270c614060d38fd224
EugeneArshinov <113054818+EugeneArshinov@users.noreply.github.com>
2022-10-04 23:45:51 +0300

    Update tmp_rfm_frequency.sql

--- tmp_rfm_frequency.sql
+++ tmp_rfm_frequency.sql
@@ -1,21 +1,21 @@
 insert into analysis.tmp_rfm_frequency
---���ҧڧ�ѧ֧� �٧ѧܧѧ٧� �٧� 2022 �ԧ�� ��� ���ѧ����� Closed
+--собираем заказы за 2022 год со статусом Closed
 with orders as ( 
 		select ord.user_id, ord.order_ts  
 			from analysis.orders ord 
 			join analysis.orderstatuses stat on ord.status = stat.id
 				where stat.key = 'Closed'
 				and ord.order_ts >= '2022-01-01'::timestamp),
---���ҧڧ�ѧ֧� �ܧ�ݧڧ�֧��ӧ� �٧ѧܧѧ٧�� �էݧ� �ܧѧاէ�ԧ� id �ܧݧڧ֧ߧ��
+--собираем количество заказов для каждого id клиента
 users as ( 
   		select ord.user_id , count(ord.*) as counted
   			from analysis.orders ord
   			group by 1),
---�է֧ݧڧ� �ߧ� 5 �ԧ���� ���� ���ާ��� ntile �ߧ� ���ߧ�ӧ� �ܧ�ݧڧ�֧��ӧ� ��է֧ݧѧߧߧ��� �٧ѧܧѧ٧��
+--делим на 5 групп при помощи ntile на основе количества сделанных заказов
 frequency as ( 
 		select counted, ntile(5) OVER( order by fr.counted ) as frequency
 			from (select counted from users group by 1) as fr )
---�٧ѧ��ݧߧ�֧� ���ݧ� �� tmp_rfm_frequency
+--заполняем поля в tmp_rfm_frequency
 select u.user_id as user_id, f.frequency 
 	from users u
 	join frequency f on f.counted = u.counted
